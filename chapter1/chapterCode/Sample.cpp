c749021889881fe08f741fa8f3286632964d51b9
JacobBatson <jacobbatson123@aol.com>
2025-11-12 18:12:55 -0500

    some of chapter 1 done

---
+++ chapter1/chapterCode/Sample.cpp
@@ -0,0 +1,14 @@
+/*
+    This is a simple cpp program
+*/
+
+#include <iostream>
+using namespace std;
+
+//A c++ program starts at main
+int main()
+{
+    cout << "C++ is power programming.";
+
+    return 0;
+}
